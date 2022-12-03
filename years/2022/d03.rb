6eec13db03ca162cf4953f208d3968631a189f30
Kris Bucyk <krisbucyk@gmail.com>
2022-12-03 11:42:23 -0500

    Refactor to use each_slice instead of manually tracking index

--- years/2022/d03.rb
+++ years/2022/d03.rb
@@ -32,12 +32,9 @@ def part2(input)
   rucksacks = []
   File.foreach(input) { |line| rucksacks << line.chomp }
   priorities_sum = 0
-  index = 0
-  while index < rucksacks.length do
-    rucksack1, rucksack2, rucksack3 = rucksacks[index...index+3]
+  rucksacks.each_slice(3) do |rucksack1, rucksack2, rucksack3|
     common_letter = (rucksack1.chars & rucksack2.chars & rucksack3.chars)[0]
     priorities_sum += letter_score(common_letter)
-    index += 3
   end
 
   priorities_sum
