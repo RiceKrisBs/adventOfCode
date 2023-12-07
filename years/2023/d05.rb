0058dbc8ded68d5b5e8e3eb607ccc5a8585fa608
Kris Bucyk <krisbucyk@gmail.com>
2023-12-06 23:55:13 -0500

    Day 5 Part 2

--- years/2023/d05.rb
+++ years/2023/d05.rb
@@ -15,26 +15,53 @@ class Day < Base
   end
 
   def part_2
-    return nil
     seed_parts, conversions = parse_input
-    seed_ranges = [] # [[79,93],[55,68]]
-    seed_parts.each_slice(2) { |a, b| seed_ranges << [a, a + b] }
-
+    seed_ranges = []
+    seed_parts.each_slice(2) { |a, b| seed_ranges << [a, a + b - 1] }
+    new_seed_ranges = nil
     conversions.each do |name, mappings| # iterate over each conversion
       new_seed_ranges = [] # accumulate the seed ranges for the next conversion
       while seed_ranges.length > 0 do # iterate over the seed ranges for the current conversion
-        range_start, range_end = seed_ranges.pop # get the bounds of the current seed range
+        seed_range_start, seed_range_end = seed_ranges.pop # get the inclusive bounds of the current seed range
+        mapping_found = false
         mappings.each do |mapping| # iterate over each mapping to see if it intersects with the current seed range
+          source_start, source_end = mapping[1], mapping[1] + mapping[2] - 1 # get the inclusive bounds of the current mapping
+          mapping_offset = mapping[1] - mapping[0]
+          # if we have a completely surrounding mapping, put the whole thing into new_seed_ranges
+          if source_start <= seed_range_start && source_end >= seed_range_end
+            new_seed_ranges << [seed_range_start - mapping_offset, seed_range_end - mapping_offset]
+            mapping_found = true
+            break
+          end
           # if we have an upper half mapping, put the upper half into new_seed_ranges and put the lower half into seed_ranges
+          if source_start <= seed_range_end && source_end >= seed_range_end
+            new_seed_ranges << [source_start - mapping_offset, seed_range_end - mapping_offset]
+            seed_ranges << [seed_range_start, source_start - 1]
+            mapping_found = true
+            break
+          end
           # if we have a lower half mapping, put the lower half into new_seed_ranges and put the upper half into seed_ranges
+          if source_start <= seed_range_start && source_end >= seed_range_start
+            new_seed_ranges << [seed_range_start - mapping_offset, source_end - mapping_offset]
+            seed_ranges << [source_end + 1, seed_range_end]
+            mapping_found = true
+            break
+          end
           # if we have a middle mapping, put the middle into new_seed_ranges and put both the upper and lower halves into seed_ranges
-          # if there is no overlap, put into seed_ranges
+          if source_start >= seed_range_start && source_end <= seed_range_end
+            new_seed_ranges << [source_start - mapping_offset, source_end - mapping_offset]
+            seed_ranges << [seed_range_start, source_start - 1]
+            seed_ranges << [source_end + 1, seed_range_end]
+            mapping_found = true
+            break
+          end
         end
-        # if there is anything left in seed_ranges, put them into new_seed_ranges
+        # put the range into new_seed_ranges if no mapping was found
+        new_seed_ranges.push([seed_range_start, seed_range_end]) unless mapping_found
       end
       seed_ranges = new_seed_ranges
     end
-    # find the smallest lower bound in seed_ranges
+    seed_ranges.map(&:first).min
   end
 
   private
